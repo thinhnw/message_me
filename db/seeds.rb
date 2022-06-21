37ed6d9ba703efb0bb8cd0bb02423fa5bc4cb14f
thinhnw <thinhnv0412@gmail.com>
2022-06-21 14:46:44 +0000

    Add message partial and refactor code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "thinhnw", password: "password")
+User.create(username: "nvt0412", password: "password")
+User.create(username: "johndoe", password: "password")
+User.create(username: "maryjane", password: "password")
