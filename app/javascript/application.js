2010fad0cc87f8aabd9b2f6040705a0188332421
thinhnw <thinhnv0412@gmail.com>
2024-08-27 14:36:34 +0000

    Switch to SemanticUI & jquery CDN

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-//= require jquery
-//= require jquery_ujs
-//= require semantic-ui
+$(document).ready(function() {
+  $('.ui.dropdown').dropdown();
+  console.log($('.ui.dropdown'))
+});
