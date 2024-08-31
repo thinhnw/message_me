d05efe337a4d803e42510fbc91f45e25307f4ab1
thinhnw <thinhnv0412@gmail.com>
2024-08-31 09:19:23 +0000

    Add chatroom channel

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "./channels"
+
 $(document).on('turbo:load', function() {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
