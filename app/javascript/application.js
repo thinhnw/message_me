6ac85e42e28a937d90d918ffa59cdcf182e44447
thinhnw <thinhnv0412@gmail.com>
2024-08-31 09:37:55 +0000

    Add method to broadcast messages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./channels"
+import "./channels/index"
 
 $(document).on('turbo:load', function() {
   $('.ui.dropdown').dropdown();
