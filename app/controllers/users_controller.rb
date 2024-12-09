fec3844f15da3f0ae392ae4c8b3ac6bbe13b31ed
thinhnw <thinhnv0412@gmail.com>
2024-12-09 06:49:48 +0000

    Implement register function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "You have successfully registered."
+      redirect_to login_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
